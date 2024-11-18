8bd3b34e1189b7a8d3ac8eb61c963f626cef0044
kyomel <michaellapandio04@gmail.com>
2024-11-18 21:57:02 +0700

    add validator

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
   task_name: string;
+
+  @IsString()
+  @IsNotEmpty()
   task_description: string;
 }
