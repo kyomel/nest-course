2558bd762132089d22344df5c2cdda7e0c10b878
kyomel <michaellapandio04@gmail.com>
2024-11-19 16:12:43 +0700

    add authentication services

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TaskModule, PrismaModule],
+  imports: [TaskModule, PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
