991a27ef13f3f1f15cc235e8a4c247498ae1716a
kyomel <michaellapandio04@gmail.com>
2024-12-10 13:52:25 +0700

    add login flow

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -10,5 +11,10 @@ export class AuthController {
   @Post('register')
   async register(@Body() data: RegisterDto) {
     return await this.authService.register(data);
+  }
+
+  @Post('login')
+  async login(@Body() data: LoginDto) {
+    return await this.authService.login(data);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,18 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RegisterDto } from './dto/register.dto';
-import { hash } from 'bcrypt';
+import { compare, hash } from 'bcrypt';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
+  /**
+   * Register
+   * @param data
+   * @returns
+   */
   async register(data: RegisterDto) {
     const checkUserExists = await this.prisma.users.findFirst({
       where: {
@@ -25,6 +31,37 @@ export class AuthService {
         statusCode: HttpStatus.OK,
         message: 'Register Successful',
       };
+    }
+  }
+
+  /**
+   * Login
+   * @param data
+   * @returns
+   */
+  async login(data: LoginDto) {
+    const checkUserExists = await this.prisma.users.findFirst({
+      where: {
+        email: data.email,
+      },
+    });
+    if (!checkUserExists) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+    const checkPassword = await compare(
+      data.password,
+      checkUserExists.password,
+    );
+    if (checkPassword) {
+      return {
+        statusCode: HttpStatus.OK,
+        message: 'Login Successful',
+      };
+    } else {
+      throw new HttpException(
+        'User or password not match',
+        HttpStatus.UNAUTHORIZED,
+      );
     }
   }
 }
